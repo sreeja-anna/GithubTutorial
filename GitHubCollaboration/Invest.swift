e3a577b8f3e7c1f651eaba4777c298dc58dc10e2
Anika Gupta <anikagupta101@gmail.com>
2025-07-29 10:14:20 -0700

    created Invest SwiftUI file

---
+++ GitHubCollaboration/Invest.swift
@@ -0,0 +1,18 @@
+//
+//  Invest.swift
+//  GitHubCollaboration
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import SwiftUI
+
+struct Invest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Invest()
+}
